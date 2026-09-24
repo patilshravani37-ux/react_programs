f4aa54b278ca32212a2ac2b1985f8b0586d275dd
patilshravani37 <patilshravani37@gmail.com>
2026-09-24 07:29:01 -0800

    commit & push

--- array_arrow.js
+++
@@ -1,4 +0,0 @@
-//square of number
-let numbers=[1,2,3,4,5];
-let squares=numbers.map(num => num*num);
-console.log(squares);
--- even.js
+++
@@ -1,3 +0,0 @@
-const isEven = num => num%2==0;
-console.log(isEven(10));
-console.log(isEven(7));
